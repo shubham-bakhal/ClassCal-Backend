a99d59ff1a2f3f2e49ad8e2165aba2db7219f074
Shubham <bakhalshubham@gmail.com>
2021-06-25 17:17:50 +0530

    check 1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://elastic-meitner-5d80ad.netlify.app/',
+      'https://elastic-meitner-5d80ad.netlify.app',
     ],
     credentials: true,
     exposedHeaders: ['set-cookie'],
